52a6236d9023439db644602273606194d732ec47
Eduardo Villas Boas <eduardovillasboassantos@gmail.com>
2019-02-22 14:31:08 -0300

    pdf
    relatorio pdf

--- src/main/java/com/gmdiias/controle/clientes/ClienteController.java
+++ src/main/java/com/gmdiias/controle/clientes/ClienteController.java
@@ -1,11 +1,31 @@
 package com.gmdiias.controle.clientes;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gmdiias.basic.BasicController;
 
 @RestController
 @RequestMapping("api/cliente")
-public class ClienteController extends BasicController<Cliente, ClienteRepository, ClienteService>{
+public class ClienteController extends BasicController<Cliente, ClienteRepository, ClienteService> {
+
+	@RequestMapping(value = "/pdfreport", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> citiesReport() throws IOException {
+		
+		ByteArrayInputStream bis = GeneratePdfReport.pdfClientes(service.findAll());
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
+				.body(new InputStreamResource(bis));
+	}
 }
